db58e694b53c14db55cc1b98ec3ed807e0bb50ab
Jeremy Evans <A02325104@usu.edu>
2024-01-23 18:27:19 -0700

    Complete

--- 1_verilog/2_operations/1_unary/src/testbench.v
+++ 1_verilog/2_operations/1_unary/src/testbench.v
@@ -28,22 +28,29 @@ module testbench ();
    // WRITE OUTPUT TO CONSOLE:
    integer fid;
    initial fid = $fopen("test_result.txt", "w");
-   
+                              // At every clock cycle, write and pring the results from 
+		 	      // the following unary operators
+			      // The output of NAND should equal OR NOT 
+			      // The output of NOR should equal AND NOT	
    always @(posedge clk) begin
       $write("clk:  %d", clk_count);      
       $write("\ta:  %b", a);
       $write("\t~a: %b", ~a);
-      $write("\t&a: %b", &a);
-      $write("\t|a: %b", |a);     
+      $write("\t~&a: %b", ~&a);         //NAND
+      $write("\t~|a: %b", ~|a);         //NOR
       $write("\t^a: %b", ^a);     
+      $write("\t&(~a): %b", &(~a));  //AND NOT
+      $write("\t|(~a): %b", |(~a));  // OR NOT
       $write("\n");
       
       $fwrite(fid,"clk:  %d", clk_count);      
       $fwrite(fid,"\ta:  %b", a);
-      $fwrite(fid,"\t~a: %b", ~a);
-      $fwrite(fid,"\t&a: %b", &a);
-      $fwrite(fid,"\t|a: %b", |a);     
+      $fwrite(fid,"\t~a: %b", ~a);       
+      $fwrite(fid,"\t~&a: %b", ~&a);     //NAND
+      $fwrite(fid,"\t~|a: %b", ~|a);     //NOR
       $fwrite(fid,"\t^a: %b", ^a);     
+      $fwrite(fid,"\t&(~a): %b", &(~a));  //AND NOT
+      $fwrite(fid,"\t|(~a): %b", |(~a));  // OR NOT
       $fwrite(fid,"\n");
    end
 
